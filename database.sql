5a46267e8c334503b957a8f483bc6092d3ccc2d9
Artem Iliushin <thatsforsure@mail.ru>
2023-11-01 14:12:52 +0300

    added url_checks table

--- database.sql
+++ database.sql
@@ -5,3 +5,15 @@ CREATE TABLE urls (
   name VARCHAR(255) UNIQUE NOT NULL,
   created_at TIMESTAMP NOT NULL
 );
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id REFERENCES urls (id),
+    status_code BIGINT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
+    created_at TIMESTAMP NOT NULL
+);
