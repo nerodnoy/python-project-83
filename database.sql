e25b4a5925524dca5be0d048a6307962347d8a46
Artem Iliushin <thatsforsure@mail.ru>
2023-11-07 16:13:52 +0300

    fixed tables

--- database.sql
+++ database.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id REFERENCES urls (id),
+    url_id BIGINT REFERENCES urls (id),
     status_code BIGINT,
     h1 VARCHAR(255),
     title VARCHAR(255),
