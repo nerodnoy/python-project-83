acf9675b21387deb6993802f667373895ec53897
Artem Iliushin <thatsforsure@mail.ru>
2023-11-07 12:25:11 +0300

    added func to get url data

--- page_analyzer/valid.py
+++ page_analyzer/valid.py
@@ -1,6 +1,8 @@
 from urllib.parse import urlparse
 from page_analyzer.db import get_urls_by_name
 import validators
+import requests
+from bs4 import BeautifulSoup
 
 
 def validate_url(url):
@@ -26,3 +28,25 @@ def validate_url(url):
     valid = {'url': url, 'error': error}
 
     return valid
+
+
+def get_url_data(url):
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        raise requests.RequestException
+
+    check = {'status_code': r.status_code}
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    h1_tag = soup.find('h1')
+    title_tag = soup.find('title')
+    description_tag = soup.find('meta', attrs={'name': 'description'})
+
+    check['h1'] = h1_tag.text.strip() if h1_tag else ''
+    check['title'] = title_tag.text.strip() if title_tag else ''
+    check['description'] = description_tag['content'].strip() \
+        if description_tag else ''
+
+    return check
