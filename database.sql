0c5ba292d6a1ac2348ab2fca76d453801663db65
Artem Iliushin <thatsforsure@mail.ru>
2023-11-08 17:13:35 +0300

    changed data types in tables

--- database.sql
+++ database.sql
@@ -12,8 +12,8 @@ CREATE TABLE url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id BIGINT REFERENCES urls (id),
     status_code BIGINT,
-    h1 VARCHAR(255),
-    title VARCHAR(255),
-    description VARCHAR(255),
+    h1 text,
+    title text,
+    description text,
     created_at TIMESTAMP NOT NULL
 );
